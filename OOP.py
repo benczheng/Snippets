6a93fb3ffec12035ce25d4de90d5ab51b80f3d3b
Ben Zheng <benzheng at berkeley>
2015-11-03 19:35:58 -0800

    Conform with PEP8 and added license

--- OOP.py
+++ OOP.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+"""OOP.py: OOP is a boilerplate template for OOP"""
+
+__author__      = "Ben Zheng"
+__copyright__   = "MIT License"
+__status__ = "Test"
+
 class myClass:
     def __init__(self, name, month, day, year, action):
         #Constructor
@@ -7,24 +15,24 @@ class myClass:
         self.action = action
 
     def do(self):
-        #Accnssor Method
+        """Accessor Method """
         return self.action
 
     def getName(self):
-        #Accessor Method that returns the name
+        """Accessor Method that returns the name"""
         return self.name
 
     def setName(self, newName):
-        #Mutator Method that change the name
+        """Mutator Method that change the name"""
         self.name = newName
 
     def birthDate(self):
-        #Accessor Method that returns string of birthdate
+        """Accessor Method that returns string of birth date"""
         return str(self.month) + "/" + str(self.day) + "/" + str(self.year)
 
     def __add__(self, other):
-        #Method overloading of the __add__ function to return the string of the action and birthday rather than the sum of two.
-        #Note: repr(x) is called when the class needs to be evaluated, whereas str(x) is a string. Example: repr(x) would return myClass('John', 1, 12, 1982, sleep) and str(x) return "John"
+        """Method overloading of the __add__ function to return the string of the action and birthday rather than the sum of two.
+        #Note: repr(x) is called when the class needs to be evaluated, whereas str(x) is a string. Example: repr(x) would return myClass('John', 1, 12, 1982, sleep) and str(x) return "John" """
         return myClass("New Class" + self.name + " and " + other.name , self.month, self.day, self.year + 1, self.action + other.action)
 
 def main():
