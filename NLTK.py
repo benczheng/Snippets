7f50efbb83ddbef10da89bf82e83630f6c33c679
Ben Zheng <benzheng at berkeley>
2015-11-04 03:34:09 -0800

    Add License

---
+++ NLTK.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+NLTK.py: NLTK has some useful functions or snippets of code used for natural language processing.
+"""
+
+__author__ = "Ben Zheng"
+__copyright__ = "MIT License"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "None"
+__maintainer__ = "Ben Zheng"
+__email__ = ""
+__status__ = "Prototype"
+
