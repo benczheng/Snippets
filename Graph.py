7624fd12c71e1e4d29abd650c13376f098555707
Ben Zheng <benjaminczheng@gmail.com>
2015-12-11 02:35:54 -0800

    Add Ising file

--- Graph.py
+++ Graph.py
@@ -12,3 +12,33 @@ __version__ = "None"
 __maintainer__ = "Ben Zheng"
 __email__ = ""
 __status__ = "Prototype"
+
+'''A Naive implementation to find seating arrangement to find the maximum permutation. '''
+def naive_max_perm(M, A=None):
+	if A is None:
+		A = set(range(len(M)))
+	if len(A) == 1: #Base Case
+		return A
+	B = set(M[i] for i in A) #Improvement here!
+	C = A - B
+	if C:
+		A.remove(C.pop())
+		return naive_max_perm(M, A)
+	return A
+
+'''A FIFO approach to maximum permutation that shoud be O(M)'''
+def max_perm(M):
+	n = len(M)
+	A = set(range(n))
+	count = [0]*n
+	for i in M:
+		count[i] += 1
+	Q = [i for i in A if count[i] == 0]
+	while Q:
+		i = Q.pop()
+		A.remove(i)
+		k = M[i]
+		count[k] -= 1
+		if count[k] == 0:
+			Q.append(k)
+	return A
